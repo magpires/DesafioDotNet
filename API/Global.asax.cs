bef11a767a49a0a30b4d4fad7fa4ed0245c0a8f5
Weverton Pires <w.m.pires@hotmail.com>
2023-10-08 18:52:06 -0300

    Adiciona swagger

--- API/Global.asax.cs
+++ API/Global.asax.cs
@@ -18,6 +18,8 @@ namespace API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var config = GlobalConfiguration.Configuration;
         }
     }
 }
