2cf44484fa085246535906023c14126a40def07c
Weverton Pires <w.m.pires@hotmail.com>
2023-10-08 23:34:39 -0300

    Adiciona conexão ao banco SQL Server

---
+++ DataAccess/Context/DesafioDotNetContext.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class DesafioDotNetContext : DbContext
+    {
+        public DesafioDotNetContext() : base("DefaultConnection")
+        {
+           
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products")
+                .HasKey(b => b.Id);
+
+            modelBuilder.Entity<Product>().Property(b => b.Brand)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            modelBuilder.Entity<Product>().Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DataAccess/Migrations/202310082356254_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Brand = c.String(nullable: false, maxLength: 256),
+                        CreatedAt = c.DateTime(nullable: false),
+                        UpdatedAt = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
